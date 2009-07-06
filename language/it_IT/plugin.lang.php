474bf2bea68d590aa0822cbe66ec3616ba66e157
riopwg <rio@piwigo.org>
2009-07-06 20:27:42 +0000

    Update lang IT
    git-svn-id: http://piwigo.org/svn/extensions/music_player@3534 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -1,5 +1,4 @@
 <?php
-
 $lang['lien_titre'] = 'Ascolta musica';
 $lang['lien_expl'] = 'Avvia un pop-up contentente il lettore audio';
 
@@ -20,6 +19,7 @@ $lang['mp_help_q4'] = "Come funziona questo plug-in?";
 $lang['mp_help_q5'] = "Come posso aggiungere link a mp3 localizzati in altre cartelle?";
 $lang['mp_help_q6'] = "E' possibile aggiungere podcast?";
 $lang['mp_help_q7'] = "Non ho trovato la risposta qui / Ho altri problemi";
+$lang['mp_help_q8'] = "Nuovo : potete gestire i diritti per ascoltare la musica";
 
 $lang['mp_help_div1'] = "Gli mp3 devono essere messi nella cartella Music del plugin come questa \"[directory di pwg]/plugins/music_player/music/[nuova playlist]/\". E' consigliato ordinare le playlists per artista (music/moby/go.pm3), o mettere il nome dell'artista direttamente nel file mp3 (music/electro/moby-go.pm3)";
 $lang['mp_help_div2'] = "Non hai aggiornato la playlist: per farlo clikka sulla 2° icona ('Gestione musica') vicino la playlist, dopo, nell'ultimo passaggio fai 'Salva modifiche'";
@@ -38,6 +38,7 @@ Per modificare una playlist :
 $lang['mp_help_div5'] = "Devi andare nella playlist che vuoi e, alla seconda tappa, aggiungere l'indirizzo del file. Attenzione: anche se l'mp3 è sullo stesso server di questo plugin, devi scrivere l'indirizzo completo che inizia con http://";
 $lang['mp_help_div6'] = "Sì: dalla versione 1.3 è stato aggiunto un blocco per i podcast e gli mp3 distanti. Attenzione: la playlist deve essere in formato Atom, Xspf (che usa il plugin), o RSS. (I podcasts sono tutti in formato RSS)";
 $lang['mp_help_div7'] = 'Puoi trovare il supporto ufficiale del plugin qui (in lingua francese) : <a href="http://forum.phpwebgallery.net/viewtopic.php?pid=68781"> http://forum.phpwebgallery.net/viewtopic.php?pid=68781</a>';
+$lang['mp_help_div8'] = "Nuovo : potete gestire i diritti per ascoltare la musica. Infatti, se create il gruppo 'music_group', solo gli utenti di questo gruppo potranno ascoltare la musica. Se il gruppo non esiste (di default), tutti avranno il diritto di accederci";
 
 $lang['mp_admin_leg'] = 'Gestione Playlists';
 $lang['mp_admin_tt_synchro'] = 'Sincronizza con la directory /music';
@@ -46,6 +47,7 @@ $lang['mp_admin_nm_pl'] = 'Nome della playlist';
 $lang['mp_admin_nm_edit'] = 'Modifica nome';
 $lang['mp_admin_pl'] = 'Gestione musica';
 $lang['mp_admin_min'] = 'Gestione immagini';
+
 $lang['mp_admin_leg1'] = 'Podcast (RSS) o link a playlist esterna';
 
 $lang['mp_admin_leg2'] = 'Opzioni del plugin';
@@ -129,5 +131,12 @@ $lang['mp_msg_admin_3_2'] = ' creato';
 $lang['mp_msg_admin_4'] = 'Aggiunta da ';
 $lang['mp_msg_admin_5'] = 'Configurazioni del lettore salvate';
 $lang['mp_msg_admin_6'] = 'Configurazioni del plugin salvate';
+
+$lang['mp_uploadimg_ok'] = 'Upload riuscito';
+$lang['mp_linkimg_ok'] = 'Immagine registrata';
+$lang['mp_info_min']="[ La miniatura è opzionale. ]";
+
+$lang['mp_text_no_autorisation']="Non siete autorizzati ad ascoltare la musica ! Per maggiori informazioni contattate l'amministratore";
+
 
 ?>
