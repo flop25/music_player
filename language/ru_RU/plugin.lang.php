a2aff37f1b1ee17ca047f08db02a9e698ad11a10
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-13 10:21:35 +0000

    [Music_player] Add ru_RU, thanks to : marchelly
    git-svn-id: http://piwigo.org/svn/extensions/music_player@14132 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,111 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['lien_expl'] = 'Показывать всплывающий встроенных плеер';
+$lang['lien_titre'] = 'Слушать Музыку';
+$lang['mp_adm_ed_sup_info'] = 'Если это файл в папке /music он будет удален, в другом случае будет удалена только запись из базы данных';
+$lang['mp_adm_ed_sup_ok'] = 'Вы уверенны?';
+$lang['mp_adm_edit_1'] = 'Описание';
+$lang['mp_adm_edit_2'] = 'Плейлист';
+$lang['mp_adm_edit_3'] = 'Синхронизировать плейлист';
+$lang['mp_adm_envoi_1'] = 'Описание';
+$lang['mp_adm_envoi_2'] = 'включает';
+$lang['mp_adm_et1_txt'] = 'Если вы добавили или удалили некоторые mp3 файлы в папке, пожалуйста синхронизируйте с базой данных.';
+$lang['mp_adm_et2_nbr'] = 'Число добавленных:';
+$lang['mp_adm_et2_rg'] = 'Вставить строку';
+$lang['mp_adm_et4_ok'] = 'Внести изменения';
+$lang['mp_adm_et4_txt'] = 'Создать playlist.xml';
+$lang['mp_adm_etape_1'] = 'Шаг 1 (необязательный)';
+$lang['mp_adm_etape_2'] = 'Шаг 2 (необязательный)';
+$lang['mp_adm_etape_3'] = 'Шаг 3';
+$lang['mp_adm_etape_4'] = 'Шаг 4';
+$lang['mp_adm_info_img'] = '(добавление нового изображение удалит предыдущее)';
+$lang['mp_adm_return_2'] = 'Назад к плейлисту';
+$lang['mp_adm_return'] = 'Назад к плейлистам';
+$lang['mp_adm_suppr_1'] = 'Что бы вы хотели сделать?';
+$lang['mp_adm_suppr_2'] = 'Удалить плейлист из базы данных (При следующей синхронизации он будет создан снова)';
+$lang['mp_adm_suppr_3'] = 'Удалить плейлист из базы данных, и папку с файлами (файлы mp3, xmp буду удалены)';
+$lang['mp_adm_thead1'] = 'ИМЯ';
+$lang['mp_adm_thead2'] = 'URL';
+$lang['mp_adm_thead3'] = 'ВВЕРХ';
+$lang['mp_adm_thead4'] = 'ВНИЗ';
+$lang['mp_adm_thead5'] = 'Изменить url/название';
+$lang['mp_adm_thead6'] = 'Удалить';
+$lang['mp_admin_2_opt2'] = 'Отображать хедер';
+$lang['mp_admin_2_opt3'] = 'Отображать нижний колонтитул, копирайт';
+$lang['mp_admin_2_opt4_1'] = 'высота';
+$lang['mp_admin_2_opt4_2'] = 'ширина';
+$lang['mp_admin_3_opt1_1'] = 'общая высота';
+$lang['mp_admin_3_opt1_2'] = 'общая ширина';
+$lang['mp_admin_3_opt2_1'] = 'высота плейлиста';
+$lang['mp_admin_3_opt3_1'] = 'Показывать миниатюру плейлиста';
+$lang['mp_admin_3_opt3_2'] = 'ширина миниатюры';
+$lang['mp_admin_3_opt4'] = 'Играть музыку в случайном порядке';
+$lang['mp_admin_3_opt5'] = 'Повторять плейлист после окончания';
+$lang['mp_admin_3_opt6'] = 'Читать этот плейлист с начала';
+$lang['mp_admin_3_opt9b'] = 'не изменять';
+$lang['mp_admin_3_opt9'] = 'Стиль по умолчанию:';
+$lang['mp_admin_folder'] = 'Папка';
+$lang['mp_admin_leg2'] = 'Настройки плагина';
+$lang['mp_admin_leg3'] = 'Параметры плеера';
+$lang['mp_admin_leg'] = 'Менеджер плейлистов';
+$lang['mp_admin_min'] = 'Укравление миниатюрами';
+$lang['mp_admin_nm_edit'] = 'Редактировать название';
+$lang['mp_admin_nm_pl'] = 'Название плейлиста';
+$lang['mp_admin_pl'] = 'Управление музыкой';
+$lang['mp_admin_tt_synchro'] = 'Синхронизировать с папкой /music';
+$lang['mp_cache'] = 'Спрятать плейлисты';
+$lang['mp_evidence_2'] = 'Запомнить';
+$lang['mp_evidence_title'] = 'Информация';
+$lang['mp_evidence_txt'] = 'В Галерее есть музыкальный плеер. Показывать его?';
+$lang['mp_help_div2'] = 'Вы не обновили плейлист: Кликните на второй икорке ("Управление музыкой") возле плейлиста, затем "Внести изменения"';
+$lang['mp_help_q1'] = 'Куда добавлять mp3';
+$lang['mp_help_q2'] = 'Я добавил mp3 но не вижу их в плейлисте!';
+$lang['mp_help_q3'] = 'Что значит \'Показывать миниатюру в плейлисте\'?';
+$lang['mp_help_q4'] = 'Как работает этот плагин?';
+$lang['mp_help_q6'] = 'Возможно-ли добавление подкастов';
+$lang['mp_help_q7'] = 'Я не нашел здесь ответ/У меня все-еще осталась проблема';
+$lang['mp_help_q8'] = 'Новое: Вы можете настроить ограничения плеера';
+$lang['mp_linkimg_ok'] = 'Изображение сохранено';
+$lang['mp_montre'] = 'Показать плейлисты';
+$lang['mp_msg_admin1'] = 'Не добавлено';
+$lang['mp_msg_admin_3_1'] = 'Файл';
+$lang['mp_msg_admin_3_2'] = 'Создан';
+$lang['mp_msg_admin_4'] = 'Добавить из';
+$lang['mp_msg_admin_5'] = 'Настройки музыкального плеера сохранены';
+$lang['mp_msg_admin_6'] = 'Настройки плагина сохранены';
+$lang['mp_msg_edit1'] = 'Редактирование успешно';
+$lang['mp_msg_edit2'] = 'Плейлист обновлен!';
+$lang['mp_msg_edit3'] = 'строка изменена!';
+$lang['mp_msg_edit4'] = 'Удалить из базы данных:';
+$lang['mp_msg_edit5'] = 'Удалить из:';
+$lang['mp_msg_err_aj1'] = 'Ошибка: поле url пустое';
+$lang['mp_msg_err_aj2'] = 'Ошибка: поле текст пустое';
+$lang['mp_msg_suppr1'] = 'Папка успешно удалена';
+$lang['mp_msg_suppr2'] = 'Ошибка: папка не удалена';
+$lang['mp_msg_suppr3'] = 'База данных успешно удалена';
+$lang['mp_need_help'] = 'Нужна помощь?';
+$lang['mp_ou'] = 'ИЛИ';
+$lang['mp_send'] = 'Отправить';
+$lang['mp_text_no_autorisation'] = 'У вас нет возможности прослушивать музыку! Пожалуйста свяжитесь с веб-мастером для выяснения дополнительной информации';
+$lang['mp_uploadimg_ok'] = 'Изображение сохранено';
+?>
