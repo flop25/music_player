921a3409a44374ac8897669b0e80ce77dc001a88
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-04-29 13:09:22 +0000

    [music_player] Add pt_PT, thanks to : klito
    git-svn-id: http://piwigo.org/svn/extensions/music_player@14522 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,127 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['lien_expl'] = 'Mostrar controle reprodutor embebido em janela pop-up';
+$lang['lien_titre'] = 'Ouvir música';
+$lang['mp_adm_ed_sup_info'] = 'Se é um arquivo da pasta \'/música\', ele será apagado: Por outro lado, apenas a entrada da base de dados será apagada.';
+$lang['mp_adm_ed_sup_ok'] = 'Tem a certeza?';
+$lang['mp_adm_edit_1'] = 'Texto descritivo';
+$lang['mp_adm_edit_2'] = 'Lista de reprodução';
+$lang['mp_adm_edit_3'] = 'Sincronizar lista de reprodução';
+$lang['mp_adm_envoi_1'] = 'Descrição de';
+$lang['mp_adm_envoi_2'] = 'Conteúdo';
+$lang['mp_adm_et1_txt'] = 'Adicionou OU removeu alguns arquivos mp3 da pasta? Se sim, você tem de sincronizar com a base de dados:';
+$lang['mp_adm_et2_nbr'] = 'Número adicionado';
+$lang['mp_adm_et2_rg2'] = '(Se em branco, base da lista de reprodução';
+$lang['mp_adm_et2_rg'] = 'inserir uma linha';
+$lang['mp_adm_et2_txt'] = 'adicionar mp3 externo';
+$lang['mp_adm_et4_ok'] = 'Submeter modificações';
+$lang['mp_adm_et4_txt'] = 'Criar uma lista de reprodução .xml';
+$lang['mp_adm_etape_1'] = '1º passo (Opcional)';
+$lang['mp_adm_etape_2'] = '2º passo ( Opcional)';
+$lang['mp_adm_etape_3'] = '3º passo!';
+$lang['mp_adm_etape_4'] = '4º passo!';
+$lang['mp_adm_info_img'] = '(adicionar nova imagem e apagar a antiga)';
+$lang['mp_adm_return_2'] = 'Voltar à lista de reprodução';
+$lang['mp_adm_return'] = 'Voltar às lista de reprodução';
+$lang['mp_adm_suppr_1'] = 'Que deseja fazer?';
+$lang['mp_adm_suppr_2'] = 'Apagar lista de reprodução da base de dados (Apenas com a sincronização seguinte, ela será criada de novo)';
+$lang['mp_adm_suppr_3'] = 'Apagar lista de reprodução da base de dados E o diretório  (arquivos mp3 e .xml serão apagados)';
+$lang['mp_adm_thead1'] = 'NOME';
+$lang['mp_adm_thead2'] = 'URL';
+$lang['mp_adm_thead3'] = 'ACIMA';
+$lang['mp_adm_thead4'] = 'ABAIXO';
+$lang['mp_adm_thead5'] = 'Alterar o nome da URL';
+$lang['mp_adm_thead6'] = 'Apagar';
+$lang['mp_admin_2_opt1'] = 'Extenção com um bloco na barra de menús';
+$lang['mp_admin_2_opt2'] = 'Mostrar o cabeçalho';
+$lang['mp_admin_2_opt3'] = 'Mostrar copyright do roda-pé';
+$lang['mp_admin_2_opt4_1'] = 'Altura';
+$lang['mp_admin_2_opt4_2'] = 'largura';
+$lang['mp_admin_2_opt4'] = 'Pou-up';
+$lang['mp_admin_3_opt10'] = 'Activar a gestão automática dos estilos: os estilos aplicados no reprodutor será o determinado pelo utilizador. Se esta opção estiver desativada ou se o estilo do utilizador não existir para o reprodutor, então é aplicado o por defeito, o antecedente';
+$lang['mp_admin_3_opt1_1'] = 'Altura total';
+$lang['mp_admin_3_opt1_2'] = 'largura total';
+$lang['mp_admin_3_opt2_1'] = 'Altura da lista de reprodução';
+$lang['mp_admin_3_opt2_2'] = '[Info:. Se miniatura é ativada, a altura da lista de reprodução a 20 px da barra, é igual à altura total]';
+$lang['mp_admin_3_opt3_1'] = 'Mostrar iniatura da lista';
+$lang['mp_admin_3_opt3_2'] = 'largura da miniatura';
+$lang['mp_admin_3_opt3_3'] = '[Info:. Se miniatura é ativada e a sua largura é <de largura total, a miniaturas será mostradaà esquerda (por defeito)]';
+$lang['mp_admin_3_opt4'] = 'Ler música em random';
+$lang['mp_admin_3_opt5'] = 'Repetir a lista depois de terminada';
+$lang['mp_admin_3_opt6'] = 'Ler esta lista do princípio';
+$lang['mp_admin_3_opt7'] = 'Executar automaticamente da lista de reprodução (sem barra lateral)';
+$lang['mp_admin_3_opt9b'] = 'Não altere';
+$lang['mp_admin_3_opt9'] = 'Estilo usado por defeito';
+$lang['mp_admin_folder'] = 'Diretório';
+$lang['mp_admin_leg1'] = 'Podcast (RSS) ou lista de reprodução distante';
+$lang['mp_admin_leg2'] = 'Opção da extenção';
+$lang['mp_admin_leg3'] = 'Opção do reprodutor';
+$lang['mp_admin_leg'] = 'Gerênciar lista de reprodução';
+$lang['mp_admin_min'] = 'Gerenciar miniatura';
+$lang['mp_admin_nm_edit'] = 'Editar nome';
+$lang['mp_admin_nm_pl'] = 'Nome da lista de reprodução';
+$lang['mp_admin_pl'] = 'Gerenciar musica';
+$lang['mp_admin_tt_synchro'] = 'Sincronizar com o diretório /music';
+$lang['mp_cache'] = 'Ocultar listas';
+$lang['mp_evidence_2'] = 'Lembrar isto';
+$lang['mp_evidence_title'] = 'Informação';
+$lang['mp_evidence_txt'] = 'A galeria tem um reprodutor de música. Deseja mostrá-lo?';
+$lang['mp_help_div1'] = 'Mp3 devem ser colocados na pasta Música da extenção como esta "[PWG folder]/plugins/music_player/music/[new playlist]/". Você pode criar a sua lista por artista (music/moby/go.pm3), ou colocar o nome dos artistas diretamente no arquivo (music/electro/moby-go.pm3)';
+$lang['mp_help_div2'] = 'Você não atualizou sua lista de reprodução: Então, clique no segundo ícone ("Gerenciar música"), junto da lista e em seguida, para terminar, clique em \'Enviar modificações\'';
+$lang['mp_help_div3'] = 'Acerca da miniatura que pode inserir (terceiro ícone("Gerenciar miniatura\'), junto da lista de reprodução';
+$lang['mp_help_div5'] = 'Sim: Para isso, vá à lista de reprodução de sua escolha e, no segundo passo, adicione o seu arquivo. Tenha cuidado: Mesmo que o mp3 esteja no mesmo servidor desta extenção, é preciso escrever a direcção completa com http://';
+$lang['mp_help_div6'] = 'Sim: Desde que um bloco 1.3 para podcast e lista tenham sido adicionados. Tenha cuidado: lista de reprodução deve estar em Atom, xspf (uso pela extenção), ou em formato RSS. (podcast são sempre RSS)';
+$lang['mp_help_div8'] = 'Novo: Pode restringir o acesso do reprodutor. Na verdade, se criar o grupo \'musica\', apenas os membros deste grupo poderão reproduzir e ouvir a música. Se o grupo não existir (caso de default), todos podem ter acesso.';
+$lang['mp_help_q1'] = 'Onde colocar mp3?';
+$lang['mp_help_q2'] = 'Adicionei mp3 mas não encontro este na lista!';
+$lang['mp_help_q3'] = 'Que quer dizer \'mostrar miniatura na lista\'?';
+$lang['mp_help_q4'] = 'Como funciona esta extenção?';
+$lang['mp_help_q5'] = 'Como posso adicionar mp3 distante ou localizado noutras pastas?';
+$lang['mp_help_q6'] = 'È possivel adicionar podcasts?';
+$lang['mp_help_q7'] = 'Não encontro aqui respostas / tenho alguns problemas';
+$lang['mp_help_q8'] = 'Novo: você pode restringir o acesso ao reprodutor';
+$lang['mp_info_min'] = '[A miniatura é opcional.]';
+$lang['mp_linkimg_ok'] = 'Foto salva';
+$lang['mp_montre'] = 'Mostrar listas';
+$lang['mp_msg_admin1'] = 'Não adicionado';
+$lang['mp_msg_admin_3_1'] = 'Arquivo';
+$lang['mp_msg_admin_3_2'] = 'Criado';
+$lang['mp_msg_admin_4'] = 'Adicionar de';
+$lang['mp_msg_admin_5'] = 'Configuração do reprodutor salva';
+$lang['mp_msg_admin_6'] = 'Configuração da extensão salva';
+$lang['mp_msg_edit1'] = 'Editar OK';
+$lang['mp_msg_edit2'] = 'Lista de reprodução actualizada';
+$lang['mp_msg_edit3'] = 'Linha modificada';
+$lang['mp_msg_edit4'] = 'Remover da base de dados';
+$lang['mp_msg_edit5'] = 'Removido de:';
+$lang['mp_msg_err_aj1'] = 'Erro: O campo URL está vazio';
+$lang['mp_msg_err_aj2'] = 'Erro: O campo texto está vazio';
+$lang['mp_msg_suppr1'] = 'Diretório apagado com sucesso';
+$lang['mp_msg_suppr2'] = 'Erro: Diretório não apagado';
+$lang['mp_msg_suppr3'] = 'Base de dados apagada com sucesso';
+$lang['mp_need_help'] = 'Necessita ajuda?';
+$lang['mp_ou'] = 'OU';
+$lang['mp_send'] = 'Enviar';
+$lang['mp_text_no_autorisation'] = 'Não tem permissão para ouvir música! Entre em contato com o webmaster para saber da razão,ou para que lhe seja permitido.';
+$lang['mp_uploadimg_ok'] = 'Foto salva';
+?>
