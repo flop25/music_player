f5ee5efeefacaab2e02660846a8ad4f449ba33f3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-02 20:18:29 +0000

    [music_player] Update cs_CZ, thanks to : sichr
    git-svn-id: http://piwigo.org/svn/extensions/music_player@14684 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,116 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['lien_expl'] = 'Zobrazit vložený rozbalovací přehrávač';
+$lang['lien_titre'] = 'Poslouchat hudbu';
+$lang['mp_adm_ed_sup_info'] = 'Pokud se jedná o soubor z hudební složky, bude smazán, jinak bude smazán pouze záznam v databázi.';
+$lang['mp_adm_ed_sup_ok'] = 'Jste si jistí?';
+$lang['mp_adm_edit_1'] = 'Popis';
+$lang['mp_adm_edit_2'] = 'Playlist';
+$lang['mp_adm_edit_3'] = 'Synchronizovat playlist';
+$lang['mp_adm_envoi_1'] = 'Popis';
+$lang['mp_adm_envoi_2'] = 'obsahuje';
+$lang['mp_adm_et1_txt'] = 'Přidali jste nebo odebrali nějaké mp3 ve složce? Pokud ano, tak musíte synchronizovat databázi:';
+$lang['mp_adm_et2_nbr'] = 'Počet přidaných:';
+$lang['mp_adm_et2_rg2'] = '(když je prázdný, jste na konci playlistu)';
+$lang['mp_adm_et2_rg'] = 'Vložit řádek:';
+$lang['mp_adm_et2_txt'] = 'Přídat vzdálenou mp3.';
+$lang['mp_adm_et4_ok'] = 'Odeslat změny';
+$lang['mp_adm_et4_txt'] = 'Uložit playlist jako .xml';
+$lang['mp_adm_etape_1'] = '1. krok (volitelný)';
+$lang['mp_adm_etape_2'] = '2. krok (volitelný)';
+$lang['mp_adm_etape_3'] = '3. krok!';
+$lang['mp_adm_etape_4'] = '4. krok!';
+$lang['mp_adm_info_img'] = '(přidat nový obrázek a starý smazat)';
+$lang['mp_adm_return_2'] = 'Zpět k playlistu';
+$lang['mp_adm_return'] = 'Zpět k playlistům';
+$lang['mp_adm_suppr_1'] = 'Co chcete udělat?';
+$lang['mp_adm_suppr_2'] = 'Smazat pouze databázi playlistu (při následné synchronizaci bude opět vytvořena)';
+$lang['mp_adm_suppr_3'] = 'Smazat databázi playlistu VČETNĚ složky (mp3 a xml soubory budou vymazány)';
+$lang['mp_adm_thead1'] = 'NÁZEV';
+$lang['mp_adm_thead2'] = 'URL';
+$lang['mp_adm_thead3'] = 'NAHORU';
+$lang['mp_adm_thead4'] = 'DOLŮ';
+$lang['mp_adm_thead5'] = 'Změnit URL/název';
+$lang['mp_adm_thead6'] = 'Smazat';
+$lang['mp_admin_2_opt2'] = 'Zobrazit "záhlaví"';
+$lang['mp_admin_2_opt3'] = 'Zobrazit "zápatí", copyright';
+$lang['mp_admin_2_opt4_1'] = 'výška';
+$lang['mp_admin_2_opt4_2'] = 'šířka';
+$lang['mp_admin_3_opt1_1'] = 'celková výška';
+$lang['mp_admin_3_opt1_2'] = 'celková šířka';
+$lang['mp_admin_3_opt2_1'] = 'výška playlistu';
+$lang['mp_admin_3_opt3_1'] = 'Zobrazit náhled playlistu';
+$lang['mp_admin_3_opt3_2'] = 'šířka náhledu';
+$lang['mp_admin_3_opt4'] = 'Náhodné načítání hudby';
+$lang['mp_admin_3_opt5'] = 'Po dohrání opakovat playlist';
+$lang['mp_admin_3_opt6'] = 'Načíst tento playlist na začátku';
+$lang['mp_admin_3_opt7'] = 'Automatické přehrávání playlistu (bez postranní lišty)';
+$lang['mp_admin_3_opt9b'] = 'neměnit';
+$lang['mp_admin_3_opt9'] = 'Standardně aplikovaný styl:';
+$lang['mp_admin_folder'] = 'Složka';
+$lang['mp_admin_leg1'] = 'Podcast (RSS) nebo vzdálený playlist';
+$lang['mp_admin_leg2'] = 'Nastavení pluginu';
+$lang['mp_admin_leg3'] = 'Nastavení playlistu';
+$lang['mp_admin_leg'] = 'Playlist manager';
+$lang['mp_admin_min'] = 'Spravovat náhledy';
+$lang['mp_admin_nm_edit'] = 'Editovat název';
+$lang['mp_admin_nm_pl'] = 'Název playlistu';
+$lang['mp_admin_pl'] = 'Spravovat hudbu';
+$lang['mp_admin_tt_synchro'] = 'Synchronizovat s hudebním adresářem';
+$lang['mp_cache'] = 'Skrýt playlisty';
+$lang['mp_evidence_2'] = 'Zapamatovat';
+$lang['mp_evidence_title'] = 'Info';
+$lang['mp_evidence_txt'] = 'Galerie má svúj přehrávač. Chcete ho zobrazit?';
+$lang['mp_help_q1'] = 'Kam uložit mp3?';
+$lang['mp_help_q2'] = 'Přidal jsem mp3, ale nezobrazují se mi v playlistu!';
+$lang['mp_help_q3'] = 'Co znamená nabídka \'Zobrazit náhledy v playlistu\'?';
+$lang['mp_help_q4'] = 'Jak tento plugin funguje?';
+$lang['mp_help_q5'] = 'Jak mohu přidat mp3, které jsou vzdálené, nebo se nacházejí v jiných složkách?';
+$lang['mp_help_q6'] = 'Je možné přidat podcasty?';
+$lang['mp_help_q7'] = 'Nenašel jsem tu odpovědi/Mám problémy';
+$lang['mp_help_q8'] = 'Nové: můžete omezit přístup přehrávače';
+$lang['mp_info_min'] = '[Náhled je volitelný]';
+$lang['mp_linkimg_ok'] = 'Obrázek uložen';
+$lang['mp_montre'] = 'Zobrazti playlisty';
+$lang['mp_msg_admin1'] = 'Nic nebylo přidáno';
+$lang['mp_msg_admin_3_1'] = 'Soubor';
+$lang['mp_msg_admin_3_2'] = 'Vytvořen';
+$lang['mp_msg_admin_4'] = 'Přidat z';
+$lang['mp_msg_admin_5'] = 'Konfigurace hudebního přehrávače uložena';
+$lang['mp_msg_admin_6'] = 'Konfigurace pluginu uložena';
+$lang['mp_msg_edit1'] = 'Úprava OK';
+$lang['mp_msg_edit2'] = 'Palylist aktualizován!';
+$lang['mp_msg_edit3'] = 'řádek změněn!';
+$lang['mp_msg_edit4'] = 'Odstranit z databáze:';
+$lang['mp_msg_edit5'] = 'Odstranit z:';
+$lang['mp_msg_err_aj1'] = 'Chyba: adresní řádek je prázdný';
+$lang['mp_msg_err_aj2'] = 'Chyba: textové pole je prázdné';
+$lang['mp_msg_suppr1'] = 'Složka byla úspěšně zmazána';
+$lang['mp_msg_suppr2'] = 'Chyba: složka nebyla smazána';
+$lang['mp_msg_suppr3'] = 'Databáze byla úspěšně zmazána';
+$lang['mp_need_help'] = 'Potřebujete pomoc?';
+$lang['mp_ou'] = 'NEBO';
+$lang['mp_send'] = 'Odeslat';
+$lang['mp_text_no_autorisation'] = 'Nemáte povoleno poslouchat hudbu! Kontaktujte prosím webmastera, aby vám vysvětlil proč a případně vám to dovolil.';
+$lang['mp_uploadimg_ok'] = 'Obrázek uložen';
+?>
