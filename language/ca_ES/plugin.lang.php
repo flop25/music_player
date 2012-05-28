3ad53509097c9ccb19b94610d3279218d940be54
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-28 12:20:45 +0000

    [music_player] Update ca_ES, thanks to : AlbertParera
    git-svn-id: http://piwigo.org/svn/extensions/music_player@15379 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ca_ES/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['lien_titre'] = 'Escoltar música';
+$lang['mp_admin_2_opt4_1'] = 'alçada';
+$lang['mp_admin_2_opt4_2'] = 'amplada';
+$lang['mp_admin_folder'] = 'Carpeta';
+$lang['mp_admin_min'] = 'Gestionar miniatures';
+$lang['mp_admin_nm_edit'] = 'Editar nom';
+$lang['mp_admin_nm_pl'] = 'Nom de la llista de reproducció';
+$lang['mp_admin_pl'] = 'Gestionar música';
+$lang['mp_cache'] = 'Ocultar llistes de reproducció';
+$lang['mp_evidence_2'] = 'Recordar';
+$lang['mp_evidence_title'] = 'Informació';
+$lang['mp_montre'] = 'Mostrar llistes de reproducció';
+$lang['mp_msg_admin1'] = 'No afegit';
+$lang['mp_msg_admin_3_1'] = 'Arxiu';
+$lang['mp_msg_admin_3_2'] = 'Creat el';
+$lang['mp_need_help'] = 'Necessita ajuda?';
+$lang['mp_send'] = 'Enviar';
+?>
