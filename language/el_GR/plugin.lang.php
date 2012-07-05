29157e7c9cebb6b780bde2449d6c0610818e3e36
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-05 15:57:43 +0000

    [music_player] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/music_player@16374 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,47 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['lien_expl'] = 'Εμφάνιση ενός αναδυόμενου ενσωματωμένου  player';
+$lang['lien_titre'] = 'Ακούστε μουσική';
+$lang['mp_adm_ed_sup_info'] = 'Εάν πρόκειται για αρχείο "φάκελος μουσικής", θα διαγράφεται: διαφορετικά, μόνο η εγγραφή στη βάσης δεδομένων θα διαγράφεται.';
+$lang['mp_adm_ed_sup_ok'] = 'Είσαι σίγουρος;';
+$lang['mp_adm_edit_1'] = 'Κείμενο ΠΕΡΙΓΡΑΦΉ';
+$lang['mp_adm_edit_2'] = 'Λίστα Αναπαραγωγής';
+$lang['mp_adm_edit_3'] = 'Συγχρονίζει τη λίστα αναπαραγωγής';
+$lang['mp_adm_envoi_1'] = 'Περιγραφή του';
+$lang['mp_adm_envoi_2'] = 'περιέχει';
+$lang['mp_adm_et1_txt'] = 'Έχετε προσθέσει ή να αφαιρέσει μερικά αρχεία mp3 στο φάκελο; Εάν ναι, πρέπει να συγχρονίσετε με τη βάση δεδομένων:';
+$lang['mp_adm_et2_nbr'] = 'Αριθμός που προσθέσατε:';
+$lang['mp_adm_et2_rg2'] = '(Εάν κενό, κάτω από τη λίστα)';
+$lang['mp_adm_et2_rg'] = 'Εισαγωγή μιας γραμμής:';
+$lang['mp_adm_et2_txt'] = 'Προσθήκη απομακρυσμένου mp3.';
+$lang['mp_adm_et4_ok'] = 'Υποβολή αλλαγών';
+$lang['mp_adm_et4_txt'] = 'Κάντε μια λίστα αναπαραγωγής .xml';
+$lang['mp_adm_etape_1'] = 'Πρώτο βήμα (προαιρετικό)';
+$lang['mp_adm_etape_2'] = '2ο βήμα (προαιρετικό)';
+$lang['mp_adm_etape_3'] = '3ο βήμα!';
+$lang['mp_adm_etape_4'] = '4ο βήμα!';
+$lang['mp_adm_info_img'] = '(Προσθήκη νέας εικόνας διαγράψει της παλιάς)';
+$lang['mp_adm_return_2'] = 'Επιστροφή  στη λίστα αναπαραγωγής';
+$lang['mp_adm_return'] = 'Επιστροφή  στις λίστες αναπαραγωγής';
+$lang['mp_adm_suppr_1'] = 'Τι θέλετε να κάνετε;';
+?>
