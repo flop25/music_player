ec8b410a95c98f4e8e89a2beabc7ee4763b1fcb6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-23 18:15:40 +0000

    [music_player] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/music_player@17631 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,127 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['mp_help_div2'] = 'Çalma listenizi güncellemeyin.';
+$lang['mp_help_div3'] = 'Çalma listesi yanına ekleyebileceğiniz 3. ikon (\'Küçük resimleri yönet\') ile ilgilidir.';
+$lang['mp_help_div8'] = 'Yeni : Müzik çalara erişimi kısıtlayabilirsiniz. Nitekim, eğer \'music_group\' gibi bir grup oluşturursanız sadece bu grup üyeleri müzik dinleyebilir. Eğer grup yoksa (varsayılan durumdur) herkes müzik çalara erişebilir.';
+$lang['mp_help_div6'] = 'Evet : 1.3 \'den beri podcast ve uzak çalma listesi için bir blok daha ilave edilmiştir. Dikkat edin  : çalma listesi mutlaka Atom, Xspf (eklenti tarafından kullanılan), yada RSS formatında olmalıdır. (podcastlar her zaman RSS)';
+$lang['mp_help_div5'] = 'Evet : Bunun için seçiminizin çalma listesine gidin ve, ikinci adımda, dosyanızı ekleyin. Dikkat edin : mp3 dosyası, eklenti ile aynı sunucuda olsa bile "http://" ile başlayan tam adresi yazmalısınız.';
+$lang['mp_help_div7'] = 'Eklentiler için buradan destek bulabilirsiniz: <a href="http://forum.phpwebgallery.net/viewtopic.php?pid=68781"> http://forum.phpwebgallery.net/viewtopic.php?pid=68781/a>';
+$lang['mp_admin_3_opt3_3'] = '[Bilgi: Eğer küçük resim etkinleştirilirse ve eğer genişliği toplam genişlikten küçük ise, küçük resim solda gözükür (varsayılan)]';
+$lang['mp_admin_3_opt2_2'] = '[Bilgi: Eğer küçük resim etkinleştirilirse, çalma listesinin yüksekliği ve çubuğun 20 pikseli toplam yüksekliğe eşit olur.]';
+$lang['mp_admin_leg1'] = 'Podcast (RSS) yada uzak çalma listesi';
+$lang['mp_help_div1'] = 'mp3 dosyaları mutlaka "[PWG folder]/plugins/music_player/music/[yeni çalma listesi]/" gibi eklentinin Music dizinine konmalıdır. Çalma listenizi sanatçıya göre (music/mogollar/agridagi.mp3) veya sanatçı adını direkt mp3 dosyasına yazarak (music/rock/mogollar-agridagi.mp3) hazırlamalısınız.';
+$lang['mp_adm_ed_sup_info'] = 'Eğer "/music" dizini dosyasıysa silinecektir: diğer durumda, sadece veri tabanı girişi silinecektir.';
+$lang['mp_adm_et1_txt'] = 'Dizine mp3 dosyası eklediniz veye sildiniz mi ? Evet ise, veri tabanını eşitlemeniz gerekmektedir.';
+$lang['mp_adm_et2_txt'] = 'Uzak mp3 ekle.';
+$lang['mp_help_q5'] = 'Diğerlerinin dizinine uzak veya yerel mp3 nasıl ekleyebilirim ?';
+$lang['mp_admin_3_opt7'] = 'Çalma listesini otomatik çalıştır (kenar çubuğu yok)';
+$lang['mp_admin_3_opt3_1'] = 'Çalma listesi küçük resmini görüntüle';
+$lang['mp_admin_3_opt3_2'] = 'küöük resim genişliği';
+$lang['mp_admin_3_opt4'] = 'Müziği rastgele oku';
+$lang['mp_admin_3_opt5'] = 'Bittiğinde çalma listesini tekrarla';
+$lang['mp_admin_3_opt6'] = 'Başlangıçta bu çalma listesini oku';
+$lang['mp_admin_3_opt9'] = 'Varsayılan olarak uygulanan stil:';
+$lang['mp_admin_2_opt1'] = 'Eklentiyi menü çubuğu bloğuna dahil et';
+$lang['mp_adm_suppr_2'] = 'Sadece veri tabanındaki çalma listesini sil (sonraki eşleştirmede tekrar yaratılacaktır)';
+$lang['mp_adm_suppr_3'] = 'Veri tabanındaki çalma listesini ve dizini sil (mp3 ve xml dosyaları silinir)';
+$lang['mp_help_q6'] = 'podcats eklemek mümkün müdür ?';
+$lang['mp_help_q2'] = 'mp3 ekledim ancak çalma listesinde bulamadım!';
+$lang['mp_help_q3'] = '"Çalma listesinde küçük resim göster" ne demek ?';
+$lang['mp_help_q1'] = 'mp3 nereye konacak ?';
+$lang['mp_help_q7'] = 'Cevapları burada bulamadım / Sorularım var';
+$lang['mp_help_q8'] = 'Yeni: müzik çalara erişimi kısıtlayabilirsiniz';
+$lang['mp_help_q4'] = 'Bu eklenti nasıl çalışır ?';
+$lang['mp_info_min'] = '[ Küçük resim isteğe bağlıdır. ]';
+$lang['mp_text_no_autorisation'] = 'Müzik dinlemenize izin verilmiyor! Webmaster ile bağlantı kurarak sebebini öğrenin veya izin alın.';
+$lang['mp_msg_suppr1'] = 'Dizin başarıyla silindi';
+$lang['mp_msg_suppr2'] = 'Hata: dizin silinemedi';
+$lang['mp_msg_suppr3'] = 'Veri tabanı başarıyla silindi';
+$lang['mp_uploadimg_ok'] = 'Resim kaydedildi';
+$lang['mp_need_help'] = 'Yardım gerekiyor mu ?';
+$lang['mp_ou'] = 'YADA';
+$lang['mp_send'] = 'Gönder';
+$lang['mp_linkimg_ok'] = 'Resim kaydedildi';
+$lang['mp_montre'] = 'Çalma listesini göster';
+$lang['mp_msg_admin1'] = 'Eklenmedi';
+$lang['mp_msg_edit1'] = 'Düzenleme tamam';
+$lang['mp_msg_err_aj1'] = 'Hata: url alanı boş';
+$lang['mp_msg_err_aj2'] = 'Hata: yazı alanı boş';
+$lang['mp_msg_edit4'] = 'Veri tabanından kaldır:';
+$lang['mp_msg_edit5'] = 'Buradan kaldır:';
+$lang['mp_msg_edit2'] = 'Çalma listesi güncellendi!';
+$lang['mp_msg_edit3'] = 'satır değiştirildi!';
+$lang['mp_msg_admin_5'] = 'Müzik çalar yapılandırması kaydedildi';
+$lang['mp_msg_admin_6'] = 'Eklenti yapılandırması kaydedildi';
+$lang['mp_msg_admin_3_1'] = 'Dosya';
+$lang['mp_msg_admin_3_2'] = 'Yaratıldı';
+$lang['mp_msg_admin_4'] = 'Eklendi';
+$lang['mp_admin_tt_synchro'] = 'Müzik dizini ile eşitle';
+$lang['mp_evidence_title'] = 'Bilgi';
+$lang['mp_evidence_txt'] = 'Galeri bir müzik çalara sahip.Göstermek istiyor musunuz ?';
+$lang['mp_cache'] = 'Çalma listesini gizle';
+$lang['mp_evidence_2'] = 'Hatırla';
+$lang['mp_admin_nm_edit'] = 'İsmi düzenle';
+$lang['mp_admin_nm_pl'] = 'Çalma listesi adı';
+$lang['mp_admin_pl'] = 'Müziği yönet';
+$lang['mp_admin_leg2'] = 'Eklenti seçeneği';
+$lang['mp_admin_leg3'] = 'Müzik çalar seçeneği';
+$lang['mp_admin_min'] = 'Küçük resim yönet';
+$lang['mp_admin_3_opt9b'] = 'değiştirmeyin';
+$lang['mp_admin_folder'] = 'Dizin';
+$lang['mp_admin_leg'] = 'Çaslma listesi yöneticisi';
+$lang['mp_adm_et4_ok'] = 'Değişiklikleri gönder';
+$lang['mp_adm_et4_txt'] = 'playlist.xml hazırla';
+$lang['mp_adm_info_img'] = '(yeni resim ekle eskisini sil)';
+$lang['mp_adm_thead6'] = 'Sil';
+$lang['mp_admin_2_opt2'] = 'Başlığı (header) göster';
+$lang['mp_admin_2_opt3'] = 'Altbilgiyi (footer) göster';
+$lang['mp_admin_2_opt4'] = 'Pop_up';
+$lang['mp_admin_2_opt4_1'] = 'yükseklik';
+$lang['mp_admin_2_opt4_2'] = 'genişlik';
+$lang['mp_admin_3_opt1_1'] = 'toplam yükseklik';
+$lang['mp_admin_3_opt1_2'] = 'toplam genişlik';
+$lang['mp_admin_3_opt2_1'] = 'çalma listesi yüksekliği';
+$lang['mp_adm_etape_1'] = '1. adım (isteğe bağlı)';
+$lang['mp_adm_etape_2'] = '2. adım (isteğe bağlı)';
+$lang['mp_adm_etape_3'] = '3. adım!';
+$lang['mp_adm_etape_4'] = '4. adım!';
+$lang['mp_adm_return'] = 'Çalma listelerine dön';
+$lang['mp_adm_return_2'] = 'Çalma listesine dön';
+$lang['mp_adm_suppr_1'] = 'Ne yapmak istiyorsunuz ?';
+$lang['mp_adm_thead1'] = 'İSİM';
+$lang['mp_adm_thead2'] = 'URL';
+$lang['mp_adm_thead3'] = 'YUKARI';
+$lang['mp_adm_thead4'] = 'AŞAĞI';
+$lang['mp_adm_thead5'] = 'url/isim değiştir';
+$lang['lien_expl'] = 'Müzik çaları açılır pencereye gömülü olarak göster';
+$lang['lien_titre'] = 'Müzlk Dinle';
+$lang['mp_adm_ed_sup_ok'] = 'Emin misiniz ?';
+$lang['mp_adm_edit_1'] = 'Açıklama yazısı';
+$lang['mp_adm_edit_2'] = 'Çalma Listesi';
+$lang['mp_adm_edit_3'] = 'Çalma listesini eşitle';
+$lang['mp_adm_envoi_1'] = 'Açıklaması';
+$lang['mp_adm_envoi_2'] = 'içermekte';
+$lang['mp_adm_et2_nbr'] = 'Eklenen adet:';
+$lang['mp_adm_et2_rg'] = 'Satır ekle:';
+$lang['mp_adm_et2_rg2'] = '(eğer boş ise, çalma listesinin altı)';
+?>
