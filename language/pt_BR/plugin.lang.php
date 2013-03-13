b93b2ce3b0a276a0d58fd032db0230a4851f697d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-13 05:39:51 +0000

    [music_player] Add pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/music_player@21527 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,123 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['mp_help_div3'] = 'Isto é sobre a miniatura que você pode inserir (ícone 3 ("Gerenciar miniatura \') perto da lista de reprodução';
+$lang['mp_help_div2'] = 'Você não atualizou sua lista: Então clique no ícone 2 ("Gerenciar música"), perto da lista de reprodução, em seguida, na etapa final, faça  \'Submeter  modificações\'';
+$lang['mp_help_q1'] = 'Onde colocar mp3?';
+$lang['mp_help_q2'] = 'Eu adicionei mp3, mas eu não consigo encontrá-los na lista de reprodução!';
+$lang['mp_help_q3'] = 'O que existe sobre \'Mostrar miniaturas em lista\'?';
+$lang['mp_help_q7'] = 'Eu não consigo encontrar respostas aqui /Eu tenho algumas problemas';
+$lang['mp_msg_admin_6'] = 'Configuração do plugin salva';
+$lang['mp_msg_edit2'] = 'Lista de reprodução atualizada';
+$lang['mp_msg_edit3'] = 'linha modificada!';
+$lang['mp_msg_edit4'] = 'Remova a partir da base de dados:';
+$lang['mp_msg_edit5'] = 'Remover a partir de:';
+$lang['mp_msg_err_aj1'] = 'Erro: campo url vazio';
+$lang['mp_msg_err_aj2'] = 'Erro: campo de texto vazio';
+$lang['mp_msg_suppr1'] = 'Pasta apagada com sucesso';
+$lang['mp_msg_suppr2'] = 'Erro: pasta não excluída';
+$lang['mp_msg_suppr3'] = 'Base de dados apagado com sucesso';
+$lang['mp_need_help'] = 'Precisa de Ajuda?';
+$lang['mp_ou'] = 'OU';
+$lang['mp_send'] = 'Enviar';
+$lang['mp_text_no_autorisation'] = 'Você não tem permissão para ouvir música! Por favor, entre em contato com o webmaster para saber por que, e talvez seja autorizado';
+$lang['mp_uploadimg_ok'] = 'Imagem salva';
+$lang['mp_help_q4'] = 'Como funciona esse plugin?';
+$lang['mp_help_q5'] = 'Como posso acrescentou mp3 distantes ou localizadas em outras pastas?';
+$lang['mp_help_q6'] = 'É possível adicionar podcats?';
+$lang['mp_msg_admin_5'] = 'Configurações do reprodutor de música salvas';
+$lang['mp_msg_edit1'] = 'Editar OK';
+$lang['mp_msg_admin_3_1'] = 'Arquivo';
+$lang['mp_msg_admin_3_2'] = 'Criado';
+$lang['mp_msg_admin_4'] = 'Adicionado de';
+$lang['mp_help_q8'] = 'Novo: você pode restringir os acessos do reprodutor';
+$lang['mp_info_min'] = '[Miniatura é opcional.]';
+$lang['mp_linkimg_ok'] = 'Salvar imagem';
+$lang['mp_montre'] = 'Mostrar lista de reprodução';
+$lang['mp_msg_admin1'] = 'Não adicionada';
+$lang['mp_evidence_txt'] = 'A galeria tem um reprodutor de música. Você quer mostrá-lo?';
+$lang['mp_admin_3_opt7'] = 'Executar automaticamente da lista (sem barra lateral)';
+$lang['mp_admin_3_opt9'] = 'Estilo aplicado por padrão:';
+$lang['mp_admin_3_opt9b'] = 'não alterar';
+$lang['mp_admin_folder'] = 'Pasta';
+$lang['mp_admin_leg'] = 'Gerenciador de lista de reprodução';
+$lang['mp_admin_leg1'] = 'Podcast (RSS) ou lista distante';
+$lang['mp_admin_leg2'] = 'Opção do plugin';
+$lang['mp_admin_leg3'] = 'Opção do tocador';
+$lang['mp_admin_min'] = 'Gerenciar miniaturas';
+$lang['mp_admin_nm_edit'] = 'Editar nome';
+$lang['mp_admin_nm_pl'] = 'Nome da lista de reprodução';
+$lang['mp_admin_pl'] = 'Gerenciar música';
+$lang['mp_admin_tt_synchro'] = 'Sincronizar com a /pasta música';
+$lang['mp_cache'] = 'Ocultar lista de reprodução';
+$lang['mp_evidence_2'] = 'Lembre-se de';
+$lang['mp_evidence_title'] = 'Informação';
+$lang['mp_admin_3_opt3_3'] = '[Informações: Se miniatura for ligada e se sua largura for < que a largura total, então as miniaturas serão mostradas à esquerda (por padrão)]';
+$lang['mp_admin_3_opt4'] = 'Ler música aleatoriamente';
+$lang['mp_admin_3_opt5'] = 'Repita lista quando terminar';
+$lang['mp_admin_3_opt6'] = 'Ler esta lista de reprodução no início';
+$lang['mp_adm_suppr_3'] = 'Excluir lista da base de dados e, a pasta (arquivos mp3, xml serão apagados)';
+$lang['mp_adm_thead1'] = 'NOME';
+$lang['mp_adm_thead2'] = 'URL';
+$lang['mp_adm_thead3'] = 'ACIMA';
+$lang['mp_adm_thead4'] = 'ABAIXO';
+$lang['mp_adm_thead5'] = 'Altere a url/nome';
+$lang['mp_adm_thead6'] = 'Excluir';
+$lang['mp_admin_2_opt1'] = 'Plugin revelado com um bloco na Barra de Menu';
+$lang['mp_admin_2_opt2'] = 'Mostrar o "cabeçalho"';
+$lang['mp_admin_2_opt3'] = 'Mostrar o "rodapé", copyright';
+$lang['mp_admin_2_opt4'] = 'Pop_up';
+$lang['mp_admin_2_opt4_1'] = 'altura';
+$lang['mp_admin_2_opt4_2'] = 'largura';
+$lang['mp_admin_3_opt10'] = 'Activar a gestão automática dos estilos: os estilos aplicados ao reprodutor  será aquele do utilizador. Se esta opção for desativada ou se o estilo do usuário não existe para o reprodutor, então o aplicado será o padrão, o precedente';
+$lang['mp_admin_3_opt1_1'] = 'altura total';
+$lang['mp_admin_3_opt1_2'] = 'largura total';
+$lang['mp_admin_3_opt2_1'] = 'altura da lista de reprodução';
+$lang['mp_admin_3_opt2_2'] = '[Info:. Se miniatura é ligada, a altura da lista de reproduçaõ e 20 px do bar, é igual à altura total]';
+$lang['mp_admin_3_opt3_1'] = 'Mostrar miniatura da lista de reprodução';
+$lang['mp_admin_3_opt3_2'] = 'largura da miniatura';
+$lang['mp_adm_info_img'] = '(adicionar uma nova imagem apagar a antiga)';
+$lang['mp_adm_return'] = 'Voltar as listas de reprodução';
+$lang['mp_adm_return_2'] = 'Voltar à lista de reprodução';
+$lang['mp_adm_suppr_1'] = 'O que você quer fazer?';
+$lang['mp_adm_suppr_2'] = 'Excluir somente a lista da base de dados (com a sincronisação seguinte, ela será criada novamente)';
+$lang['lien_expl'] = 'Mostrar pop-up de um tocador de música embutido';
+$lang['lien_titre'] = 'Ouvir Música';
+$lang['mp_adm_ed_sup_info'] = 'Se for um arquivo da pasta \'/música\', ele será excluído: em outro caso, somente as entradas no banco de dados serão apagadas.';
+$lang['mp_adm_ed_sup_ok'] = 'Você tem certeza?';
+$lang['mp_adm_edit_1'] = 'Texto descrição';
+$lang['mp_adm_edit_2'] = 'Lista para tocar';
+$lang['mp_adm_edit_3'] = 'Sincronizar a lista para tocar';
+$lang['mp_adm_envoi_1'] = 'Descrição de';
+$lang['mp_adm_envoi_2'] = 'contém';
+$lang['mp_adm_et1_txt'] = 'Voce adicionou ou removeu alguns arquivos mp3 da pasta? Se sim, você tem que sincronizar com a base de dados:';
+$lang['mp_adm_et2_nbr'] = 'Número de adição:';
+$lang['mp_adm_et2_rg'] = 'Inserir uma linha:';
+$lang['mp_adm_et2_rg2'] = '(se vazia, embaixo da lista de reprodução)';
+$lang['mp_adm_et2_txt'] = 'Adicione mp3 distante.';
+$lang['mp_adm_et4_ok'] = 'Submeter modificações';
+$lang['mp_adm_et4_txt'] = 'Faça uma lista de reprodução .xml';
+$lang['mp_adm_etape_1'] = 'O primeiro passo (opcional)';
+$lang['mp_adm_etape_2'] = '2º passo (optionnal)';
+$lang['mp_adm_etape_3'] = '3º passo!';
+$lang['mp_adm_etape_4'] = '4º passo!';
+?>
