0106d29781683a24b775e5c2bcc81c1d6d77e317
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-30 11:05:12 +0000

    [music_player] Add zh_CN, thanks to : dennisyan
    git-svn-id: http://piwigo.org/svn/extensions/music_player@24069 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,63 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['mp_admin_3_opt10'] = '启用自动风格管理：播放器的风格将视用户而定。如不启用本选项，或不存在用户选定的风格，则将应用之前的预设。';
+$lang['mp_admin_2_opt1'] = '在菜单顶部以专门的区块突出显示插件';
+$lang['mp_adm_suppr_3'] = '删除数据库中的播放列表，并删除所有文件（mp3，xml文件将被删除）';
+$lang['mp_adm_suppr_2'] = '仅删除数据库中的播放列表（下次同步时将重新生成）';
+$lang['mp_admin_3_opt1_1'] = '总高度';
+$lang['mp_adm_info_img'] = '（添加一张新图片，覆盖旧图片）';
+$lang['mp_adm_return'] = '返回播放列表';
+$lang['mp_adm_return_2'] = '返回播放列表';
+$lang['mp_adm_suppr_1'] = '您想做什么？';
+$lang['mp_adm_thead1'] = '名称';
+$lang['mp_adm_thead2'] = 'url';
+$lang['mp_adm_thead3'] = '上';
+$lang['mp_adm_thead4'] = '下';
+$lang['mp_adm_thead5'] = '更改url/名称';
+$lang['mp_adm_thead6'] = '删除';
+$lang['mp_admin_2_opt2'] = '显示页头"header"';
+$lang['mp_admin_2_opt3'] = '显示页脚"footer"，copyright';
+$lang['mp_admin_2_opt4'] = 'Pop_up';
+$lang['mp_admin_2_opt4_1'] = '高';
+$lang['mp_admin_2_opt4_2'] = '宽';
+$lang['mp_adm_et1_txt'] = '如果您在文件夹中添加或删除过mp3文件，则有必要同步数据库/文件：';
+$lang['mp_adm_et2_txt'] = '添加或远程mp3。';
+$lang['mp_adm_et2_nbr'] = '添加总数：';
+$lang['mp_adm_et2_rg'] = '插入行：';
+$lang['mp_adm_et2_rg2'] = '（留空，则在播放列表底部）';
+$lang['mp_adm_et4_ok'] = '提交修改';
+$lang['mp_adm_et4_txt'] = '生成 .xml 播放列表';
+$lang['mp_adm_etape_1'] = '第1步（可选）';
+$lang['mp_adm_etape_2'] = '第2步（可选）';
+$lang['mp_adm_etape_3'] = '第3步！';
+$lang['mp_adm_etape_4'] = '第4步！';
+$lang['mp_adm_ed_sup_ok'] = '确定？';
+$lang['mp_adm_edit_1'] = '描述文字';
+$lang['mp_adm_edit_2'] = '播放列表';
+$lang['mp_adm_edit_3'] = '同步播放列表';
+$lang['mp_adm_envoi_1'] = '描述';
+$lang['mp_adm_envoi_2'] = '包含';
+$lang['mp_adm_ed_sup_info'] = '如果它是\'/music\'文件夹中的文件，则将被删除：如果不是，则只有数据库条目将被删除。';
+$lang['lien_titre'] = '听音乐';
+$lang['lien_expl'] = '显示弹出的嵌入式播放器';
+?>
